01a7635ace89d1d517334396813332dde181d288
cylammarco <cylammarco@gmail.com>
2021-12-08 19:20:41 +0200

    fixed separators with os.sep

--- shared_folder/generate_modified_fsps_miles_template_spectra.py
+++ shared_folder/generate_modified_fsps_miles_template_spectra.py
@@ -8,28 +8,27 @@ import ppxf as ppxf_package
 import fsps
 from spectres import spectres
 
-
 ppxf_dir = os.path.dirname(os.path.realpath(ppxf_package.__file__))
-miles_pathname = ppxf_dir + '/miles_models/Mun1.30*.fits'
-output_folder_pathname = ppxf_dir + '/fsps_miles_models'
+miles_pathname = ppxf_dir + os.sep + 'miles_models' + os.sep + 'Mun1.30*.fits'
+output_folder_pathname = ppxf_dir + os.sep + 'fsps_miles_models'
 
 if not os.path.exists(output_folder_pathname):
     os.mkdir(output_folder_pathname)
 
 miles_filelist = glob.glob(miles_pathname)
 
-
 sp = fsps.StellarPopulation(zcontinuous=1, sfh=0)
 
 for i in miles_filelist:
-    filename = i.split('\\')[-1]
+    filename = i.split(os.sep)[-1]
     metallicity = filename.split('Z')[-1].split('T')[0]
     if metallicity[0] == 'p':
         metallicity = float(metallicity[1:])
     elif metallicity[0] == 'm':
         metallicity = -float(metallicity[1:])
     else:
-        print('This should not happen. Check the filename: {}.'.format(filename))
+        print(
+            'This should not happen. Check the filename: {}.'.format(filename))
     age = float(filename.split('T')[-1].split('_iPp')[0])
     miles_data = fits.open(i)[0]
     w_start = float(miles_data.header['CRVAL1'])
@@ -41,7 +40,7 @@ for i in miles_filelist:
     flux_model = miles_data.data
     '''
     sp.params['logzsol'] = metallicity
-    wave_fsps, flux_fsps =  sp.get_spectrum(tage=age, peraa=True)
+    wave_fsps, flux_fsps = sp.get_spectrum(tage=age, peraa=True)
     flux_fsps = spectres(wave_model, wave_fsps, flux_fsps)
     '''
     figure(1)
